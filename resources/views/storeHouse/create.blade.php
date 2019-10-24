0257182cb0cf969e535299efb43f3d57f12e2f81
Antony Abel Montalvo Garcia <antonis162010@gmail.com>
2019-10-24 01:41:07 -0500

    Algunos cambios

--- resources/views/storeHouse/create.blade.php
+++ resources/views/storeHouse/create.blade.php
@@ -6,14 +6,14 @@
 <script type="text/javascript" src="{{ asset('js/storeHouseForm.js') }}"></script>
 @endsection
 @section('assets')
-{{-- <link href="{{ asset('css/home.css') }}" rel="stylesheet"> --}}
+<link href="{{ asset('css/storeHouse.css') }}" rel="stylesheet">
 @endsection
 
 @section('content')
 <br />
 <h2 class="p-l-30">
     <i onclick="location.href='javascript:history.back()';" class="fas fa-arrow-left p-r-30">
-    </i>Agregar libro
+    </i>Agregar producto al almacén
 </h2>
 <br />
 @component('component.formStoreHouse', [
